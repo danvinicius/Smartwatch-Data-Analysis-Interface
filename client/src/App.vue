755e047c1f76db9c4fb2a65403741ccb9b65d09b
Daniel Vinicius <viniccius774@gmail.com>
2023-12-01 11:25:54 -0300

    feat: first commit

---
+++ client/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <TheHeader></TheHeader>
+  <PeriodSelector></PeriodSelector>
+  <CardiacData></CardiacData>
+  <Steps></Steps>
+  <SleepQuality></SleepQuality>
+  <Bioimpedance></Bioimpedance>
+  <TheFooter></TheFooter>
+</template>
+
+<script setup lang="ts">
+import TheHeader from './components/layout/TheHeader.vue'
+import TheFooter from './components/layout/TheFooter.vue'
+import PeriodSelector from './components/PeriodSelector.vue';
+import CardiacData from './components/CardiacData.vue';
+import Steps from './components/Steps.vue';
+import SleepQuality from './components/SleepQuality.vue';
+import Bioimpedance from './components/Bioimpedance.vue';
+</script>
+
+<style scoped>
+</style>
